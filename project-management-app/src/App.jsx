8ec47f92c658d0580c7e8a468eaf1dcb446e357d
peihsuan-lin <lin.peih@northeastern.edu>
2024-12-22 14:15:09 -0800

    add sidebar and startporject feature

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,7 +1,46 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import React, { useState } from "react";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  function handleAddProject(project) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...project,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+        {content}
+      </main>
     </>
   );
 }
